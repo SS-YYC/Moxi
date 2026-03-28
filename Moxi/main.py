85a00458ddba0138103e372ca9afce166d23730c
KerbalMissile <kerbalmissile@gmail.com>
2026-03-28 13:44:47 -0600

    Delete Moxi/main.py

--- Moxi/main.py
+++
@@ -1,8 +0,0 @@
-import customtkinter as ctk
-from ui import MoxiApp
-
-if __name__ == "__main__":
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("dark-blue")
-    app = MoxiApp()
-    app.mainloop()
